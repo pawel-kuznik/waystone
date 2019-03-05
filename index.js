d9fce6561899daac43c01b9d56cce56efb42a4ff
pawel <pawel.kuznik@gmail.com>
2019-03-05 23:24:35 +0100

    An initial implementation for the router and the route.

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ *  The kickstart file for the library.
+ *  @author     Pawel Kuznik <pawel.kuznik@gmail.com>
+ */
+
+// expose the library
+module.exports = {
+    Route:  require('./lib/Route.js'),
+    Router: require('./lib/Router.js')
+};
